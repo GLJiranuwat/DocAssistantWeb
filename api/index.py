8902c4a2a822a31d085dde08b098e6d156acd1d9
Jiranuwat <jiranuwat.tima@graphlogic.ai>
2025-04-29 14:15:47 +0700

    remove handler

--- api/index.py
+++ api/index.py
@@ -5,7 +5,3 @@ app = Flask(__name__, static_folder="../static", template_folder="../templates")
 @app.route("/")
 def home():
     return render_template("index.html")
-
-# ✅ Required for Vercel
-def handler(environ, start_response):
-    return app.wsgi_app(environ, start_response)
